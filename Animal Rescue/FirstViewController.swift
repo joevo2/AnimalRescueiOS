90924a3508a58c298fecf203d2bca3a99386a9ef
Joel Yek <Joel@Joels-MacBook-Air.local>
2014-10-08 11:58:30 +0800

    Initial Commit

---
+++ Animal Rescue/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Animal Rescue
+//
+//  Created by Joel Yek on 10/8/14.
+//  Copyright (c) 2014 Joel Yek. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
